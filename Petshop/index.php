40198f27bc65079291cbab786fa849af76d4dbbe
Lau-szutner <lautaroszutner@gmail.com>
2024-04-25 22:08:28 -0300

    agrego imagenes

--- Petshop/index.php
+++ Petshop/index.php
@@ -6,7 +6,7 @@
     <title>Programacion II Parcial 1</title>
     <link rel="stylesheet" href="">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./img/style.css">
+    <link rel="stylesheet" href="./styles/style.css">
     
 </head>
 <body class="">
@@ -33,28 +33,13 @@
     </header>
   
     <main>
-            <?php
-        // __DIR__ es una "constante mágica" de php.
-        // Esta constante retorna la ruta absoluta a la carpeta del archivo en el que estoy.
-        // Cuando hacemos requires o includes, siempre es una buena idea usar __DIR__ para la
-        // ruta.
-        require __DIR__ . "/views/home.php";
+        <?php
+            require __DIR__ . "/views/home.php";
         ?>
 
-        <main class="container-fluid">
-
-</main>
     </main>
 
-
-
-<?php
-
-
-?>
-
     <footer>
-    
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
--- Petshop/views/home.php
+++ Petshop/views/home.php
@@ -1,8 +1,10 @@
-<main class="container-fluid">
-    <section>
-        <h2>Productos</h2>
-        <p>Compra todo para tus amigos de 4 patas</p>
-        <img src="" alt="">
-        <p>Hola Lau me lees?</p>
+<main class="container">
+    <section class="container-fluid d-flex justify-content-center flex-lg-row flex-sm-column " >
+        <div class="d-flex flex-column justify-content-center alig-items-center mt-5">
+            <h2>Productos</h2>
+            <p>Compra todo para tus amigos de 4 patas</p>
+            <button class="btn btn-warning w-100" >Comprar</button>
+        </div>
+        <img src="img/pedigree.jpeg" alt="Foto publicitaria de Pedigree"  class="col-lg-6">
     </section>
 </main>
