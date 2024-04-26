2a502de4f3bdce0231ed3802023928b965155adc
Lau-szutner <lautaroszutner@gmail.com>
2024-04-25 23:16:32 -0300

    agrego tarjetas y mejoro la parte responsive

--- Petshop/views/home.php
+++ Petshop/views/home.php
@@ -1,10 +1,49 @@
-<main class="container">
-    <section class="container-fluid d-flex justify-content-center flex-lg-row flex-sm-column " >
-        <div class="d-flex flex-column justify-content-center alig-items-center mt-5">
-            <h2>Productos</h2>
-            <p>Compra todo para tus amigos de 4 patas</p>
-            <button class="btn btn-warning w-100" >Comprar</button>
+
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> -->
+
+
+<main>
+
+<section class="container d-flex justify-content-center flex-lg-row flex-column align-items-center">
+    <div class="text-center">
+        <h2>Productos</h2>
+        <p>Compra todo para tus amigos de 4 patas</p>
+        <button class="btn btn-warning w-100">Comprar</button>
+    </div>
+    <img src="img/pedigree.jpeg" alt="Foto publicitaria de Pedigree" class="col-lg-6 mt-5 mt-lg-0 img-fluid">
+</section>
+
+    <section class="container-fluid bg-warning py-5">
+        <div class="container">
+            <h2 class="text-center mb-4">Servicios</h2>
+            <div class="row justify-content-center">
+                <div class="col-lg-4 mb-4">
+                    <div class="card text-center p-3 h-100 d-flex flex-column">
+                        <h3>Veterinaria</h3>
+                        <img src="img/vete.jpg" alt="Foto de veterinaria" class="img-fluid mb-3 rounded-2">
+                        <p>Los servicios veterinarios son esenciales para el bienestar de nuestras mascotas. Desde chequeos regulares hasta tratamientos especializados, los veterinarios brindan atención experta para mantener a nuestros amigos peludos saludables y felices.</p>
+                        <button class="btn btn-dark mt-auto">Pedir</button>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card text-center p-3 h-100 d-flex flex-column">
+                        <h3>Guardería</h3>
+                        <img src="img/guarderia.jpg" alt="Foto de guardería" class="img-fluid mb-3 rounded-2">
+                        <p>Nuestras guarderías ofrecen un ambiente seguro y divertido para que tus mascotas socialicen y se diviertan mientras estás fuera. Contamos con personal capacitado y instalaciones adecuadas para brindarles la mejor atención.</p>
+                        <button class="btn btn-dark mt-auto">Pedir</button>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card text-center p-3 h-100 d-flex flex-column">
+                        <h3>Estilista Canino</h3>
+                        <img src="img/peluqueria.webp" alt="Foto de estilista canino" class="img-fluid mb-3 rounded-2">
+                        <p>Nuestros estilistas caninos son expertos en mantener a tu peludo amigo luciendo su mejor aspecto. Desde baños y cortes de pelo hasta tratamientos de belleza, garantizamos que tu mascota se sienta y se vea genial.</p>
+                        <button class="btn btn-dark mt-auto">Pedir</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <img src="img/pedigree.jpeg" alt="Foto publicitaria de Pedigree"  class="col-lg-6">
     </section>
 </main>
+
+
