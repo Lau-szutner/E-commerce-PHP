d344e6faf3df5287d9269ac9539f4c68bfa8c337
Lau-szutner <lautaroszutner@gmail.com>
2024-04-30 12:03:17 -0300

    subo cambios esteticos

--- Petshop/index.php
+++ Petshop/index.php
@@ -2,31 +2,31 @@
 
 // whitelist
 $rutas = [
-    'home' => [
-        'titulo' => 'Programacion II Parcial 1',
-    ],
-    'productos' => [
-        'titulo' => 'Productos',
-    ],
-    'servicios' => [
-        'titulo' => 'Nuestros Servicios',
-    ],
-    'contacto' => [
-        'titulo' => 'Contactanos',
-    ],
-    '404' => [
-        'titulo' => 'Página no Encontrada',
-    ],
-    'detalleProducto' => [
-      'titulo' => 'Detalle del Producto',
-    ],
-    ];
+  'home' => [
+    'titulo' => 'Programacion II Parcial 1',
+  ],
+  'productos' => [
+    'titulo' => 'Productos',
+  ],
+  'servicios' => [
+    'titulo' => 'Nuestros Servicios',
+  ],
+  'contacto' => [
+    'titulo' => 'Contactanos',
+  ],
+  '404' => [
+    'titulo' => 'Página no Encontrada',
+  ],
+  'detalleProducto' => [
+    'titulo' => 'Detalle del Producto',
+  ],
+];
 
 $vista = $_GET['seccion'] ?? 'home';
 
 // Verificamos si la vista que nos están pidiendo se permite.
 if (!isset($rutas[$vista])) {
-    $vista = '404';
+  $vista = '404';
 }
 
 // Obtenemos las opciones/configuración de la ruta que corresponden a esta vista.
@@ -35,61 +35,76 @@ $rutaConfig = $rutas[$vista];
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $rutaConfig['titulo']; ?></title>
-    <link rel="stylesheet" href="./styles/normalize.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="icon" href="./img/icons/favicon.png" >
-    
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $rutaConfig['titulo']; ?></title>
+  <link rel="stylesheet" href="./styles/normalize.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="icon" href="./img/icons/favicon.png">
+  <link rel="stylesheet" href="css/productos.css">
 </head>
+
 <body class="">
 
-    <header>
-      <nav class="container-fluid d-flex flex-wrap align-items-center justify-content-center py-3 mb-4 bg-warning">
-        <div class="col-md-3 mb-2 mb-md-0">
-          <a href="index.php?seccion=home" class="d-inline-flex link-body-emphasis text-decoration-none fs-5">
-            <img src="img/logoInvertido.png" alt="logo">
-          </a>
+  <header>
+    <nav class="navbar navbar-expand-md fixed-top px-5">
+      <div class="container-fluid d-flex justify-content-between">
+        <a href="index.php?seccion=home" class="d-inline-flex link-body-emphasis text-decoration-none fs-5">
+          <img src="img/logoInvertido.png" alt="logo">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon" id="ol"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mx-auto">
+            <li><a href="index.php?seccion=home" class="nav-link px-2 text-black">Home</a></li>
+            <li><a href="index.php?seccion=productos" class="nav-link px-2 text-black">Productos</a></li>
+            <li><a href="index.php?seccion=servicios" class="nav-link px-2 text-black">Servicios</a></li>
+            <li><a href="index.php?seccion=contacto" class="nav-link px-2 text-black">Contacto</a></li>
+          </ul>
         </div>
 
-        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="index.php?seccion=home" class="nav-link px-2 text-black">Home</a></li>
-          <li><a href="index.php?seccion=productos" class="nav-link px-2 text-black">Productos</a></li>
-          <li><a href="index.php?seccion=servicios" class="nav-link px-2 text-black">Servicios</a></li>
-          <li><a href="index.php?seccion=contacto" class="nav-link px-2 text-black">Contacto</a></li>
-        </ul>
-
-        <div class="col-md-3 text-end"> 
-          <button type="button" class="btn btn-outline-dark me-2">Login</button>
+        <div id="login">
+          <form class="d-flex"> <!-- Utilizamos ml-auto para enviar el botón a la izquierda -->
+            <button class="btn btn-outline-dark ">Login</button>
+          </form>
         </div>
-      </nav>
-    </header>
-  
-    <main>
-        <?php
-          require __DIR__.'/views/'.$vista.'.php';
-?>
 
-    </main>
-
-    <footer id="footer" >
-      <div class="infoFooter">
-          <picture>
-            <img src="img/logo2.png" alt="logo">
-          </picture>
-          <p>PAW Petshop<p>
-          <div class="iconos">
-            <a href="#"><img src="img/icons/facebook.png" alt="icono de facebook"></a>
-            <a href="#"><img src="img/icons/instagram.png" alt="icono de instagram"></a>
-            <a href="#"><img src="img/icons/youtube.png" alt="icono de youtube"></a>
-          </div>
-        </div>
       </div>
-      
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </nav>
+
+
+  </header>
+
+  <main>
+    <?php
+    require __DIR__ . '/views/' . $vista . '.php';
+    ?>
+
+  </main>
+
+  <footer id="footer">
+    <div class="infoFooter d-flex flex-column align-items-center p-5">
+      <picture>
+        <img src="img/logo2.png" alt="logo">
+      </picture>
+      <p>PAW Petshop
+      <p>
+      <div class="iconos d-flex gap-5">
+        <a href="#"><img src="img/icons/facebook.png" alt="icono de facebook"></a>
+        <a href="#"><img src="img/icons/instagram.png" alt="icono de instagram"></a>
+        <a href="#"><img src="img/icons/youtube.png" alt="icono de youtube"></a>
+      </div>
+    </div>
+    </div>
+
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- Petshop/views/productos.php
+++ Petshop/views/productos.php
@@ -1,47 +1,47 @@
 <?php
 // require __DIR__.'/../data/productos.php';
-require __DIR__.'/../biblioteca/productos.php';
+require __DIR__ . '/../biblioteca/productos.php';
 $productos = productosTodos();
 ?>
 
 
+<section id="sectionProductos" class="container-fluid d-flex flex-column align-items-center">
+    <h1>Productos</h1>
 
-<h1 class="text-center">Productos</h1>
-<section id="sectionProductos" class="container">
-    <div class="row row-cols-1 row-cols-md-3">
-    
-    <?php
-    foreach ($productos as $producto) { // acá irian los dos puntos del foreach pero debo tener activado una extension porque VSC me lo autocorrige
+    <div class="container row row-cols-1 row-cols-sm-2 row-cols-md-3 ">
+        <?php
+        foreach ($productos as $producto) { // acá irian los dos puntos del foreach pero debo tener activado una extension porque VSC me lo autocorrige
         ?>
-        <div class="col"> 
-            <div class="card">
-                <article class="contenidoCard">
-                    <picture class="item">
-                        <img src=<?php echo $producto['imagen']; ?> alt="<?php echo $producto['nombre']; ?>"> 
-                    </picture>                
-                    <a href="index.php?seccion=detalleProducto&id=<?php echo $producto['id']; ?>">
-                        <h2> <?php echo $producto['nombre']; ?> </h2>
-                    </a>
-                    <div>
-                        <p><?php echo $producto['descripcion']; ?> </p>
-                        <p><?php echo '$'.$producto['precio']; ?></p>
-                        <p><?php if ($producto['disponibilidad']) {
-                            echo 'DISPONIBLE';
-                        } else {
-                            echo 'SIN STOCK';
-                        } ?> </p>
-                    </div>
-                
-                    <a href="index.php?seccion=detalleProducto&id=<?php echo $producto['id']; ?>"><button>Ver Detalle</button></a>
-                    <button>Comprar</button>
-                </article>
+            <div class="col">
+                <div class="card">
+                    <article class="contenidoCard">
+                        <picture class="item">
+                            <img src=<?php echo $producto['imagen']; ?> alt="<?php echo $producto['nombre']; ?>">
+                        </picture>
+                        <a href="index.php?seccion=detalleProducto&id=<?php echo $producto['id']; ?>">
+                            <h2> <?php echo $producto['nombre']; ?> </h2>
+                        </a>
+                        <div>
+                            <p><?php echo $producto['descripcion']; ?> </p>
+                            <p><?php echo '$' . $producto['precio']; ?></p>
+                            <p><?php if ($producto['disponibilidad']) {
+                                    echo 'DISPONIBLE';
+                                } else {
+                                    echo 'SIN STOCK';
+                                } ?> </p>
+                        </div>
+
+                        <button class="productos-btn"><a href="index.php?seccion=detalleProducto&id=<?php echo $producto['id']; ?>">Ver Detalle</a></button>
+
+                        <button class="productos-btn">Comprar</button>
+                    </article>
+                </div>
             </div>
-        </div>
-        
 
-    <?php
-    } // esto sería el endforeach pero debo tener activado una extension porque VSC me lo autocorrige
-?>
 
-</div>
+        <?php
+        } // esto sería el endforeach pero debo tener activado una extension porque VSC me lo autocorrige
+        ?>
+
+    </div>
 </section>
