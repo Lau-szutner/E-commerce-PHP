2f01b2832b244c4277c9f4988e11fa95c06efcd0
Lau-szutner <lautaroszutner@gmail.com>
2024-05-01 20:35:45 -0300

    agrego la seccion servicios

--- Petshop/views/servicios.php
+++ Petshop/views/servicios.php
@@ -1,31 +1,83 @@
 <section id="servicios-container">
     <div class="container ">
         <h1 class="text-center p-5">Servicios</h1>
-        <div class="row justify-content-center">
-            <div class="col-lg-4 mb-4">
-                <div class="card text-center p-3 h-100 d-flex flex-column">
-                    <h3>Veterinaria</h3>
-                    <img src="img/vete.jpg" alt="Foto de veterinaria" class="img-fluid mb-3 rounded-2">
-                    <p>Los servicios veterinarios son esenciales para el bienestar de nuestras mascotas. Desde chequeos regulares hasta tratamientos especializados, los veterinarios brindan atención experta para mantener a nuestros amigos peludos saludables y felices.</p>
-                    <button class="btn btn-dark mt-auto">Pedir</button>
+        <div class="d-flex gap-2 flex-column">
+            <div class="col-12 mb-4 serviciosCard card text-center p-3 h-100 d-flex flex-colum">
+                <h3>Veterinaria</h3>
+                <div class="d-flex flex-lg-row flex-column">
+                    <img src="img/vete.jpg" alt="Foto de veterinaria" class="img-fluid mb-3 rounded-2 col-12 col-lg-5  ">
+                    <div class="p-3 d-flex flex-column justify-content-evenly">
+                        <p>Los servicios veterinarios son esenciales para el bienestar de nuestras mascotas. Desde chequeos regulares hasta tratamientos especializados, los veterinarios brindan atención experta para mantener a nuestros amigos peludos saludables y felices.</p>
+                        <ul class="serviciosLista">
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet</li>
+                        </ul>
+                    </div>
                 </div>
+                <a href="index.php?seccion=contacto"> <button class="btn btn-dark mt-auto w-100">Pedir</button></a>
             </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card text-center p-3 h-100 d-flex flex-column">
-                    <h3>Guardería</h3>
-                    <img src="img/guarderia.jpg" alt="Foto de guardería" class="img-fluid mb-3 rounded-2">
-                    <p>Nuestras guarderías ofrecen un ambiente seguro y divertido para que tus mascotas socialicen y se diviertan mientras estás fuera. Contamos con personal capacitado y instalaciones adecuadas para brindarles la mejor atención.</p>
-                    <button class="btn btn-dark mt-auto">Pedir</button>
+            <div class="col-12 mb-4 serviciosCard card text-center p-3 h-100 d-flex flex-colum">
+                <h3>Guardería</h3>
+                <div class="d-flex flex-lg-row flex-column">
+                    <img src="img/guarderia.jpg" alt="Foto de veterinaria" class="img-fluid mb-3 rounded-2 col-12 col-lg-5  ">
+                    <div class="p-3 d-flex flex-column justify-content-evenly">
+                        Nuestras guarderías ofrecen un ambiente seguro y divertido para que tus mascotas socialicen y se diviertan mientras estás fuera. Contamos con personal capacitado y instalaciones adecuadas para brindarles la mejor atención.</p>
+                        <ul class="serviciosLista">
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet</li>
+                        </ul>
+                    </div>
                 </div>
+                <a href="index.php?seccion=contacto"> <button class="btn btn-dark mt-auto w-100">Pedir</button></a>
             </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card text-center p-3 h-100 d-flex flex-column">
-                    <h3>Estilista Canino</h3>
-                    <img src="img/peluqueria.webp" alt="Foto de estilista canino" class="img-fluid mb-3 rounded-2">
-                    <p>Nuestros estilistas caninos son expertos en mantener a tu peludo amigo luciendo su mejor aspecto. Desde baños y cortes de pelo hasta tratamientos de belleza, garantizamos que tu mascota se sienta y se vea genial.</p>
-                    <button class="btn btn-dark mt-auto">Pedir</button>
+            <div class="col-12 mb-4 serviciosCard card text-center p-3 h-100 d-flex flex-colum">
+                <h3>Estilista Canino</h3>
+                <div class="d-flex flex-lg-row flex-column">
+                    <img src="img/peluqueria.webp" alt="Foto de veterinaria" class="img-fluid mb-3 rounded-2 col-12 col-lg-5  ">
+                    <div class="p-3 d-flex flex-column justify-content-evenly">
+                        <p>Nuestros estilistas caninos son expertos en mantener a tu peludo amigo luciendo su mejor aspecto. Desde baños y cortes de pelo hasta tratamientos de belleza, garantizamos que tu mascota se sienta y se vea genial.</p>
+                        <ul class="serviciosLista">
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet</li>
+                        </ul>
+                    </div>
                 </div>
+                <a href="index.php?seccion=contacto"> <button class="btn btn-dark mt-auto w-100">Pedir</button></a>
+            </div>
+            <div class="col-12 mb-4 serviciosCard card text-center p-3 h-100 d-flex flex-colum">
+                <h3>Adiestramiento canino</h3>
+                <div class="d-flex flex-lg-row flex-column">
+                    <img src="img/adiestramiento.jpg" alt="Foto de veterinaria" class="img-fluid mb-3 rounded-2 col-12 col-lg-5  ">
+                    <div class="p-3 d-flex flex-column justify-content-evenly">
+                        <p>Nuestros estilistas caninos son expertos en mantener a tu peludo amigo luciendo su mejor aspecto. Desde baños y cortes de pelo hasta tratamientos de belleza, garantizamos que tu mascota se sienta y se vea genial.</p>
+                        <ul class="serviciosLista">
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="index.php?seccion=contacto"> <button class="btn btn-dark mt-auto w-100">Pedir</button></a>
+            </div>
+            <div class="col-12 mb-4 serviciosCard card text-center p-3 h-100 d-flex flex-colum">
+                <h3>Transporte de mascotas</h3>
+                <div class="d-flex flex-lg-row flex-column">
+                    <img src="img/transporte.jpg" alt="Foto de veterinaria" class="img-fluid mb-3 rounded-2 col-12 col-lg-5  ">
+                    <div class="p-3 d-flex flex-column justify-content-evenly">
+                        <p>Nuestros estilistas caninos son expertos en mantener a tu peludo amigo luciendo su mejor aspecto. Desde baños y cortes de pelo hasta tratamientos de belleza, garantizamos que tu mascota se sienta y se vea genial.</p>
+                        <ul class="serviciosLista">
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet </li>
+                            <li>Lorem ipsum dolor sit, amet</li>
+                        </ul>
+                    </div>
+                </div>
+                <a href="index.php?seccion=contacto"> <button class="btn btn-dark mt-auto w-100">Pedir</button></a>
             </div>
         </div>
+    </div>
     </div>
 </section>
