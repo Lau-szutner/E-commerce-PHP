5b9c6a27543e480d610272099c537c6c591cecae
MariaMacarenaMerlo <macarena.merlo@gmail.com>
2024-06-29 18:33:33 -0300

    Create producto.php
    clase

---
+++ Petshop/clases/producto.php
@@ -0,0 +1,14 @@
+<?php 
+class Producto 
+// definit que tipo de datos es cada uno? 
+{
+    public $id;
+    public $nombre; 
+    public $categoria; 
+    public $descripcion;
+    public $precio;
+    public $disponibilidad; 
+    public $imagen; 
+    public $cuerpo;
+}
+?>
