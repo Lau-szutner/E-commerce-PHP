076d7d07f191f933e04085cb1b627ef59639e751
MariaMacarenaMerlo <macarena.merlo@gmail.com>
2024-07-01 12:47:10 -0300

    base de datos

---
+++ database.php
@@ -0,0 +1,41 @@
+<?php
+
+// datos de la base 
+
+const DB_SERVER = "localhost";
+const DB_USER = "root";
+const DB_PASS = "root";
+const DB_NAME = "dw3_fernandezszutner_merlo";
+
+const DB_DSN = "mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+
+// conexion 
+try {
+    //objeto de la clase PDO
+    $conn = new PDO(DB_DSN, DB_USER, DB_PASS);
+
+
+    //configurar el modo de excepcion
+    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "conexion establecida";
+
+} catch (PDOException $error) {
+    echo 'error de conexion: ' . $error->getMessage();
+}
+
+
+
+$consulta = "SELECT * FROM producto";
+
+//modo preparacion 
+
+$stmt = $conn->prepare($consulta);
+$stmt -> execute();
+
+print_r($stmt);
+
+
+
+?>
+
+
