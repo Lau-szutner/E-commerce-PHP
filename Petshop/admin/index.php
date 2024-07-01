33103703e4cd012a831e9a721e3c336c8b899ecf
MariaMacarenaMerlo <macarena.merlo@gmail.com>
2024-07-01 19:15:30 -0300

    cambios

--- Petshop/admin/index.php
+++ Petshop/admin/index.php
@@ -1,18 +1,15 @@
 <?php
 
-// whitelist
+//copia del index
 $rutas = [
-  'home' => [
-    'titulo' => 'Programacion II Parcial 1',
+  'dashboard' => [
+    'titulo' => 'Panel',
   ],
   'productos' => [
     'titulo' => 'Productos',
-  ],
-  'servicios' => [
-    'titulo' => 'Nuestros Servicios',
-  ],
-  'contacto' => [
-    'titulo' => 'Contactanos',
+    ],
+  'iniciar-sesion' => [
+    'titulo' => 'Ingresar al panel',
   ],
   '404' => [
     'titulo' => 'Página no Encontrada',
@@ -42,9 +39,9 @@ $rutaConfig = $rutas[$vista];
   <title><?php echo $rutaConfig['titulo']; ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-  <link rel="icon" href="./img/icons/favicon.png">
+  <link rel="icon" href="../img/icons/favicon.png">
 
-  <link rel="stylesheet" href="./css/home.css">
+  <link rel="stylesheet" href="../css/home.css">
 
 </head>
 
---
+++ Petshop/admin/views/dashboard.php
@@ -0,0 +1,4 @@
+<section>
+    <h1>Panel de control</h1>
+    <h2>Bienvenido al panel de control</h2>
+</section>
