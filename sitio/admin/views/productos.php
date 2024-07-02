bbb1426a106b77f49dd0ba9f9e990ea4aecb0014
MariaMacarenaMerlo <macarena.merlo@gmail.com>
2024-07-01 21:30:44 -0300

    Update productos.php

--- sitio/admin/views/productos.php
+++ sitio/admin/views/productos.php
@@ -3,36 +3,45 @@
 //require __DIR__.'/../biblioteca/productos.php';
 //$productos = productosTodos();
 
-require_once __DIR__ . '/../clases/Producto.php';
+require_once __DIR__ . '/../../clases/Producto.php';
 $productosObj = new Producto();
 $productos = $productosObj->productosTodos();
 ?>
 
 <section id="sectionProductos" class="container-fluid d-flex flex-column align-items-center">
-    <h1 class="p-5">Productos</h1>
+    <h1 class="p-5">Administracion de productos </h1>
 
-    <div class="container row row-cols-1 row-cols-sm-2 row-cols-md-3">
-        <?php foreach ($productos as $producto) : ?>
-            <div class="col">
-                <div class="card">
-                    <article class="contenidoCard">
-                        <picture class="item">
-                            <img src="<?php echo $producto->getImagen(); ?>" alt="<?php echo $producto->getNombre(); ?>">
-                        </picture>
-                        <a class="link" href="index.php?seccion=detalleProducto&id=<?php echo $producto->getProducto_Id(); ?>">
-                            <h2><?php echo $producto->getNombre(); ?></h2>
-                        </a>
-                        <div>
-                            <p><?php echo $producto->getDescripcion(); ?></p>
-                            <p><?php echo '$'.$producto->getPrecio(); ?></p>
-                            <p><?php echo $producto->getDisponibilidad() ? 'DISPONIBLE' : 'SIN STOCK'; ?></p>
-                        </div>
-                        <a class="productos-btn text-center" href="index.php?seccion=detalleProducto&id=<?php echo $producto->getProducto_Id(); ?>">Ver Detalle</a>
-                        <button class="productos-btn">Comprar</button>
-                    </article>
-                </div>
-            </div>
-        <?php endforeach; ?>
+    <div class="row">
+        <table class="col table"> 
+            <thead>   
+                <tr>
+                    <th>Id</th>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Disponibilidad</th>
+                    <th>Descripción</th>
+                    <th>Imagen</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead> 
+            <tbody>
+                <?php 
+                foreach ($productos as $producto) :
+                ?>
+                <tr>
+                    <td><?= $producto->getProducto_id(); ?> </td>
+                    <td><?= $producto->getNombre(); ?> </td>
+                    <td><?= $producto->getPrecio(); ?> </td>
+                    <td><?= $producto->getDisponibilidad(); ?> </td>
+                    <td><?= $producto->getDescripcion(); ?> </td>
+                    <td><img src= " <?= '../img/' . $producto->getImagen();?>" alt="" </td>
+                    <td> Ya lo vamos a hacer&trade; </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody> 
+
+            
+        </div>
     </div>
 </section>
 
