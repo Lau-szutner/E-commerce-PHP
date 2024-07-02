3d1362006999b9c05590063127db43025a35ca73
MariaMacarenaMerlo <macarena.merlo@gmail.com>
2024-07-02 14:47:06 -0300

    Create producto-nuevo.php

---
+++ sitio/admin/views/producto-nuevo.php
@@ -0,0 +1,27 @@
+<section class="container">
+    <h1>Añadir nuevo producto</h1>
+    <form action="#" method="post" ></form>
+        <label for="titulo">Título</label>
+        <input type="text" id="titulo" name="titulo">
+
+        <label for="descripcion">Descripción</label>
+        <input type="text" id="descripcion" name="descripcion">
+
+        <label for="cuerpo">Cuerpo</label>
+        <input type="text" id="cuerpo" name="cuerpo">
+
+        <label for="precio">Precio</label>
+        <input type="number" id="precio" name="precio">
+        
+        <label for="disponibilidad">Disponibilidad</label>
+        <input type="radio" id="disponibilidad" name="disponibilidad">
+
+        <label for="imagen">Imagen</label>
+        <input type="file" name="imagen" id="imagen">
+
+        <label for="img_descripcion">imagen descripcion</label>
+        <input type="text" name="img_descripcion" id="img_descripcion">
+
+        <button type="submit" class="button">Añadir</button>    
+    </form>
+</section>
