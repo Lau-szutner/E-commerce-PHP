41835863c8825b0a72b8a569c9b42a38521ca059
Lau-szutner <lautaroszutner@gmail.com>
2024-07-11 18:18:21 -0300

    cambios estetico y responsive

--- sitio/views/404.php
+++ sitio/views/404.php
@@ -1,3 +1,23 @@
-<div id="caja">
+<div id="" class="container d-flex flex-column align-items-center p-5">
     <h1>ERROR 404</h1>
+
+    <p>¡Oh no! Página no encontrada
+    </p>
+    <p>Lo sentimos, pero la página que estás buscando no existe.
+        En Paw Petshop, nuestra prioridad es asegurarnos de que tú y tus mascotas tengan la mejor experiencia posible. Sin embargo, parece que algo salió mal y no pudimos encontrar la página que buscabas.
+    </p>
+    <p> Aquí tienes algunas opciones para seguir navegando:
+        Página Principal: Vuelve a nuestra página principal y descubre todo lo que Paw Petshop tiene para ofrecer.
+        Productos: Explora nuestra amplia variedad de productos para tus mascotas.
+        Servicios: Descubre los servicios que ofrecemos para cuidar y mimar a tus amigos peludos.
+        Contacto: Ponte en contacto con nosotros para cualquier consulta o asistencia que necesites.</p>
+
+    <ul class="d-flex gap-5 list-unstyled mb-0 flex-column flex-lg-row">
+        <li><a href="index.php?seccion=home" class="btn btn-warning btn-rounded text-black">Home</a></li>
+        <li><a href="index.php?seccion=productos" class="btn btn-warning btn-rounded text-black">Productos</a></li>
+        <li><a href="index.php?seccion=servicios" class="btn btn-warning btn-rounded text-black">Servicios</a></li>
+        <li><a href="index.php?seccion=contacto" class="btn btn-warning btn-rounded text-black">Contacto</a></li>
+    </ul>
+
+
 </div>
