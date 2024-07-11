18092b7c2e01091efbfd3c462e79e13e62d9b71c
MariaMacarenaMerlo <macarena.merlo@gmail.com>
2024-07-11 20:40:43 -0300

    dashboard estilizado

--- sitio/admin/views/dashboard.php
+++ sitio/admin/views/dashboard.php
@@ -1,4 +1,35 @@
-<section>
-    <h1>Panel de control</h1>
-    <h2>Bienvenido al panel de control</h2>
+<section class="container mb-5 pt-5">
+    <div class="row"></div>
+    <h1 class="text-center pt-5">Bienvenido al Panel de Administración de Paw Petshop</h1>
+    <h2 class="text-center">Gestiona tu tienda de mascotas de manera eficiente y rápida.</h2>
+    <article class="row text-center d-flex justify-content-center mt-5">
+        <div class="col-md-6 col-lg-3 mb-4">
+            <div class="card h-100 shadow p-3 mb-5 bg-body-tertiary rounded">
+                <div class="card-body">
+                    <h2 class="card-title">Productos</h2>
+                    <p class="card-text">Gestiona tus productos, añade nuevos y actualiza la información existente.</p>
+                    <a href="index.php?seccion=productos" class="btn btn-dark">Ver Productos</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-3 mb-4">
+            <div class="card h-100 shadow p-3 mb-5 bg-body-tertiary rounded">
+                <div class="card-body">
+                    <h2 class="card-title">Categorías</h2>
+                    <p class="card-text">Administra las categorías de productos para una mejor organización.</p>
+                    <a href="index.php?seccion=categorias" class="btn btn-dark">Ver Categorías</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-3 mb-4">
+                <div class="card h-100 shadow p-3 mb-5 bg-body-tertiary rounded">
+                    <div class="card-body">
+                        <h2 class="card-title">Usuarios</h2>
+                        <p class="card-text">Gestiona los usuarios registrados y sus roles.</p>
+                        <a href="usuarios.php" class="btn btn-dark">Ver Usuarios</a>
+                    </div>
+                </div>
+            </div>
+
+    </article>
 </section>
