b21e1ccbe784d5b5789bb1183a35cd584e28c984
lau-szutner <lautaroszutner@gmail.com>
2025-10-28 20:38:06 -0300

    arreglo espacios de form

--- sitio/views/contacto.php
+++ sitio/views/contacto.php
@@ -1,29 +1,41 @@
 <section class="container p-5 d-flex flex-column">
     <h2 class="text-center">Contacto</h2>
     <div class="row row-cols-1 row-cols-md-2 ">
-        <form action="procesar.php" method="get" class="col d-flex flex-column mb-5" id="contactoForm">
-            <label for="nombre">Nombre:</label>
-            <input type="text" id="nombre" name="nombre" required>
+        <form action="procesar.php" method="get" class="col d-flex flex-column mb-5 gap-4 p-3" id="contactoForm">
+            
+            <label for="nombre" class="d-flex flex-column">
+                Nombre:
+                <input type="text" id="nombre" name="nombre" required>
+            </label>
 
-            <label for="email">E-mail:</label>
-            <input type="email" id="email" name="email" required>
+            <label for="email" class="d-flex flex-column">
+                E-mail:
+                <input type="email" id="email" name="email" required>
+            </label>
 
-            <label for="tel">Teléfono:</label>
-            <input type="tel" id="tel" name="tel" required>
+            <label for="tel" class="d-flex flex-column">
+                Teléfono:
+                <input type="tel" id="tel" name="tel" required>
+            </label>
 
-            <label for="motivo">¿Por qué nos contactas?</label>
-            <select id="motivo" name="motivo">
-                <option value=".">Elegi tu motivo de contacto</option>
-                <option value="consulta">Consulta</option>
-                <option value="Pedido">Pedido</option>
-                <option value="Servicio">Servicios</option>
-                <option value="Reclamo">Reclamos</option>
-                <option value="otros">Otros</option>
-            </select>
+            <label for="motivo" class="d-flex flex-column">
+                ¿Por qué nos contactas?
+                <select id="motivo" name="motivo" required>
+                    <option value="" disabled selected>Elegí tu motivo de contacto</option>
+                    <option value="consulta">Consulta</option>
+                    <option value="pedido">Pedido</option>
+                    <option value="servicio">Servicios</option>
+                    <option value="reclamo">Reclamos</option>
+                    <option value="otros">Otros</option>
+                </select>
+            </label>
 
-            <label for="mensaje">Mensaje:</label>
-            <textarea id="mensaje" name="mensaje" rows="4" cols="50" required></textarea>
-            <button type="submit">Enviar</button>
+            <label for="mensaje" class="d-flex flex-column">
+                Mensaje:
+                <textarea id="mensaje" name="mensaje" rows="4" cols="50" required></textarea>
+            </label>
+
+            <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
 
         <picture class="col ">
